6747eccf9f3b52861dbf498f635a3682fdc44984
YifanWang0 <ywang00@stuy.edu>
2019-03-13 02:12:49 -0400

    Element class

---
+++ Element.java
@@ -0,0 +1,11 @@
+public class Element {
+  public int intElem;
+  public double doubleElem;
+  public String stringElem;
+
+  public Element(int intElem, double doubleElem, String stringElem) {
+    intElem = this.intElem ;
+    doubleElem = this.doubleElem;
+    stringElem = this.stringElem;
+  }
+}
